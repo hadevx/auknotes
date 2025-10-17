e98d6f682dcde2d4c53ebbd54f46d3ee28885753
Hussain <hkosaimi@gmail.com>
2025-10-17 21:01:57 +0300

    new

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import { Heart } from "lucide-react";
 import { motion } from "framer-motion";
-import { VideoText } from "./ui/video-text";
+
 export default function Footer() {
   return (
     <footer className="bg-neutral-900 px-10">
