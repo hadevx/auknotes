679c83bf9577695e11dc2a40527f5e64d687d289
Hussain <hkosaimi@gmail.com>
2025-11-06 08:31:22 +0300

    new

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./", // <-- ensures correct relative paths for deployment
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
